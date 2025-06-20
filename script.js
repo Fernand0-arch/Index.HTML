3c423581140fd0749b61f0794797391cea097584
Fernand0-arch <fernando_st3@hotmail.com>
2025-06-20 20:34:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ /*Programação do botão*/
+ function mensagem() {
+ let p = document.getElementById("b1");
+ if (p.textContent === "") {p.textContent = "Ola Mundo!";}
+ else {p.textContent = "";}}
+
+
